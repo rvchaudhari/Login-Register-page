c8d39a504149541e0f5aa36be8f290cbf480a793
RV chaudhari <116835106+rvchaudhari@users.noreply.github.com>
2023-03-06 21:22:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let header = document.querySelector("header");
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+// Dark Mode / light mode
+let darkmode = document.querySelector("#darkmode");
+
+darkmode.onclick = () => {
+  if (darkmode.classList.contains("bx-moon")) {
+    darkmode.classList.replace("bx-moon", "bx-sun");
+    document.body.classList.add("active");
+  } else {
+    darkmode.classList.replace("bx-sun", "bx-moon");
+    document.body.classList.remove("active");
+  }
+};
